b71399384869d287508a37ec52095a90e7104daa
Joyce Rocha <106878476+joyce-rocha@users.noreply.github.com>
2023-05-27 17:11:22 -0400

    Add files via upload

---
+++ LoginForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ email: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Login(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-inner">
+        <h2>Probare</h2>
+        {error !== "" ? <div className="error">{error}</div> : ""}
+        <div className="form-group">
+          <label htmlFor="email">Login</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+        <input type="submit" value="ENTRAR" />
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
